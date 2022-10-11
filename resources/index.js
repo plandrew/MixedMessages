b3ca6cd7272a0620f64b78de0dd72641e2033045
Bennett Humphrey <bennetthumphrey98@gmail.com>
2022-10-11 14:29:29 -0500

    add js and html files
    add random number generator in js, and boilerplate html

---
+++ resources/index.js
@@ -0,0 +1,13 @@
+
+
+// for randomNumber(3), for example, it will output any number from 1-3
+
+const randomNumber = n => {
+    return Math.ceil(Math.random()*n)
+}
+
+console.log(randomNumber(2))
+console.log(randomNumber(3))
+console.log(randomNumber(4))
+console.log(randomNumber(5))
+console.log(randomNumber(10))
